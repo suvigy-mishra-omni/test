ae9df3600f06adc31be0d4558bc327bef25ccf54
Suvigy Mishra <suvigy.mishra@saisonomni.com>
2024-12-05 16:57:46 +0530

    Init

---
+++ src/main/java/com/example/test/TestApplication.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class TestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestApplication.class, args);
+  }
+}
